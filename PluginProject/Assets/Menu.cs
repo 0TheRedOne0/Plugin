989776e9c3cdb13e5e355c1211f36f5c241aeeee
0TheRedOne0 <d34d.m4n.4rt@gmail.com>
2022-10-20 17:13:31 -0500

    commit 1

---
+++ PluginProject/Assets/Menu.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Menu 
+{
+    [MenuItem("Plantilla/Función If-Else", false, 1)]
+
+    private static void NewMenuOption()
+    {
+
+    }
+
+    [MenuItem("Plantilla/Función If-Then", false, 2)]
+
+    private static void NewMenuOption2()
+    {
+
+    }
+
+    [MenuItem("Plantilla/Función Quit", false, 3)]
+
+    private static void NewMenuOption3()
+    {
+
+    }
+
+    [MenuItem("Plantilla/Variable/ Entero(int)", false, 4)]
+
+    private static void NewMenuOption4()
+    {
+
+    }
+
+    [MenuItem("Plantilla/Variable/ flotante (float)", false, 4)]
+
+    private static void NewMenuOption5()
+    {
+
+    }
+
+    [MenuItem("Plantilla/Variable/ Booleano (bool)", false, 4)]
+
+    private static void NewMenuOption6()
+    {
+
+    }
+
+
+    [MenuItem("Plantilla/Operaciones/ Suma ", false, 5)]
+
+    private static void NewMenuOption7()
+    {
+
+    }
+
+    [MenuItem("Plantilla/Operaciones/ Resta ", false, 5)]
+
+    private static void NewMenuOption8()
+    {
+
+    }
+
+    [MenuItem("Plantilla/Operaciones/ Multiplicación ", false, 5)]
+
+    private static void NewMenuOption9()
+    {
+
+    }
+
+    [MenuItem("Plantilla/Operaciones/ División ", false, 5)]
+
+    private static void NewMenuOption10()
+    {
+
+    }
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
